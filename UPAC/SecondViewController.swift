61aba6ef3016bd34dcbe0b60239f6d9eeb9ccc7d
Marquez <rmarquez14@rmarquez14m41.local>
2014-10-09 21:35:48 -0500

    Initial Commit

---
+++ UPAC/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  UPAC
+//
+//  Created by Marquez, Richard A on 10/9/14.
+//  Copyright (c) 2014 wsu-cs-club. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
